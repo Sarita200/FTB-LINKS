57dbd1b90552ad99960028d5f29098df4374e33e
Sarita200 <saritasadgir1@gmail.com>
2024-08-03 11:55:24 +0530

    link form created

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,12 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
+import axios from "axios"
+import toast from "react-hot-toast"
+import { Toaster } from 'react-hot-toast'
 
 function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    const [linkData, setLink] = useState({
+        title: "",
+        target: "",
+        slug: ""
+    })
+    const createLink = async (req, res) => {
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/link`, linkData)
+        if (response.data.success) {
+            toast.success("Shorten Link Successfully...")
+
+            setLink({
+                title: "",
+                target: "",
+                slug: ""
+            })
+        }
+        else {
+            toast.error(response.data.message)
+        }
+        console.log(response)
+    }
+    return (
+        <div >
+            <h1 className='Title'> Shorten Link..🤩</h1>
+            <h2 className='title-heading'></h2>
+            <form className='link-form'>
+                <input
+                    type='text'
+                    placeholder='Title'
+                    value={linkData.title}
+                    onChange={(e) => {
+                        setLink({
+                            ...linkData,
+                            title: e.target.value
+                        })
+                    }}
+                    className='Link-Input'
+                />
+
+                <input
+                    type='text'
+                    placeholder='Target'
+                    value={linkData.target}
+                    onChange={(e) => {
+                        setLink({
+                            ...linkData,
+                            target: e.target.value
+                        })
+                    }}
+                    className='Link-Input'
+                />
+
+                <input
+                    type='text'
+                    placeholder='slug'
+                    value={linkData.slug}
+                    onChange={(e) => {
+                        setLink({
+                            ...linkData,
+                            slug: e.target.value
+                        })
+                    }}
+                    className='Link-Input'
+                />
+
+                <button
+                    type='button'
+                    onClick={createLink}
+                    className='link-btn'
+                >Shorten</button>
+
+
+            </form>
+            <Toaster/>
+        </div>
+    )
 }
 
 export default Home
