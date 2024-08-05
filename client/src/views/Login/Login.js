5083cd808b9c71ef1fa50037e6d0029c21774969
Sarita200 <saritasadgir1@gmail.com>
2024-08-05 15:40:28 +0530

    login form created

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import './Login.css'
 
 function Login() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <form className='formContainer'>
+                <h1 className='form-title'> Login</h1>
+
+                <div className="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control input-box" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control input-box" id="exampleInputPassword1" placeholder="Password" />
+                </div>
+
+                <button type="button" className="signUp-btn">Login</button>
+            </form>
+        </div>
+    )
 }
 
 export default Login
