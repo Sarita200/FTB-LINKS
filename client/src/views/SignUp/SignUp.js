c7261af1277a64afc30d4493693654d548048116
Sarita200 <saritasadgir1@gmail.com>
2024-08-05 16:46:08 +0530

    signUp connected to database

--- client/src/views/SignUp/SignUp.js
+++ client/src/views/SignUp/SignUp.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignUp.css'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { Link } from 'react-router-dom'
+import axios from "axios"
+import toast from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 
 function SignUp() {
+
+    const [ user, setUser]  = useState({
+        fullName: '',
+        email: '',
+        password: '',
+        proffession: ''
+    })
+
+    const signUp = async () => {
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/signUp`, {
+            fullName: user.fullName,
+            email: user.email,
+            password: user.password,
+            proffession: user.proffession
+        })
+
+        if (response.data.success) {
+            toast.success(response.data.message)
+
+            setUser({
+                fullName: '',
+                email: '',
+                password: '',
+                proffession: ''
+            })
+        }
+        else {
+            toast.error(response.data.message)
+        }
+        console.log(response)
+    }
     return (
         <div>
             <form className='formContainer'>
@@ -11,12 +45,17 @@ function SignUp() {
 
                 <div className="form-group">
                     <label for="exampleInputEmail1">Full Name</label>
-                    <input type="text" className="form-control input-box" id="Full Name" aria-describedby="Full Name" placeholder="Full Name" />
+                    <input type="text" className="form-control input-box" id="Full Name" aria-describedby="Full Name" placeholder="Full Name"
+                        value={user.fullName}
+                        onChange={(e) => { setUser({ ...user, fullName: e.target.value }) }} />
                 </div>
 
                 <div class="form-group">
-                    <label for="exampleFormControlSelect1">Profession</label >
-                    <select className="form-control input-box" id="exampleFormControlSelect1">
+                    <label for="exampleInputEmail1">Profession</label >
+                    <select className="form-control input-box" id="exampleFormControlSelect1"
+                    value={user.proffession}
+                    onChange={(e) => { setUser({ ...user, proffession: e.target.value }) }}
+                        >
                         <option>Profession</option>
                         <option>Business</option>
                         <option>Learning</option>
@@ -26,24 +65,28 @@ function SignUp() {
 
                 <div className="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control input-box" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <input type="email" className="form-control input-box" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"
+                        value={user.email}
+                        onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
+                    
                 </div>
 
 
                 <div className="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control input-box" id="exampleInputPassword1" placeholder="Password" />
+                    <input type="password" className="form-control input-box" id="exampleInputPassword1" placeholder="Password"
+                        value={user.password}
+                        onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
                 </div>
 
-                <div className="form-group form-check">
-                    <input type="checkbox" className="form-check-input input-box" id="exampleCheck1" />
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="button" className="signUp-btn">SignUp</button>
+                <button type="button"
+                    onClick={signUp}
+                    className="signUp-btn">SignUp</button>
 
                 <Link to='/login' className=" alreadyAcc">Already Have an account ? <span className='login-text' > Login</span></Link>
+                <Toaster />
             </form>
+            
         </div>
     )
 }
