d851d0e29a0d36b366ba920615cb13c6240f2136
Sarita200 <saritasadgir1@gmail.com>
2024-08-06 12:51:45 +0530

    icons added

--- client/src/components/linkCards/linkCard.js
+++ client/src/components/linkCards/linkCard.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import './LinkCard.css'
 import CopyImg from './../LinkCards/link.png'
+import targetImg from './../LinkCards/link2.png'
+import slugImg from './../LinkCards/link3.png'
+import viewImg from './../LinkCards/eye.png'
 
 function LinkCard({ title, target, slug, views, createdAt }) {
   const shortUrl = `${process.env.REACT_APP_API_URL}/${slug}`
- 
+
   return (
     <div className='link-cards'>
-     <span> <img src={CopyImg} className='copyImg' /><span></span></span>
+      <span> <img src={CopyImg} className='copyImg' /><span></span></span>
       <h3 className='link-card-title'>{title || 'No title'}</h3>
-      
-     <p className='link-card-target'> ShortUrl :<a href={shortUrl} target='_blank' >{shortUrl}</a></p>
 
-     <p className='link-card-target'> TargetUrl :<a></a></p>
+      <p >
+        <img src={slugImg} className='shortImg' />
+        <a href={shortUrl} target='_blank' className='link-card-slug'>{shortUrl}</a></p>
+
+      <p > 
+        <img src={targetImg} className='targetImg'/>
+        <a href={target} target='_blank' className='link-card-target'>
+          {target?.substring(0, 40)}{target?.length > 50 ? "..." : null}
+        </a>
+      </p>
 
       <p className='link-card-view'>
-        <span className='link-card-view-text'>views : 
+        <span className='link-card-view-text'>
+          <img src={viewImg} className='eye-view'/>
           {views > 0 ? `${views} people visited` : `Share to view count`}
         </span>
       </p>
